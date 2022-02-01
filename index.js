bc9565705f090354e95347e3bccb2144012f4a94
mbanda1 <mbandanixon@gmail.com>
2022-02-02 01:09:16 +0300

    Add express json, cors, routes, error handler and middlewares

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
+require('dotenv').config();
+
 const express = require('express');
+const cors = require('cors');
+const configs = require('./config/envroment-variables.json');
+const errorHandler = require('./middleware/errorHandler');
+const errors = require('./errors');
+const routes = require('./routes');
 
 const app = express();
-const port = 3000;
+
+const jsonParser = express.json();
+const corsMiddleware = cors();
+
+app.use(corsMiddleware);
+app.use(jsonParser);
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+app.use(routes);
+
+app.use(errorHandler.internalServerErrorHandler(errors));
+app.use(errorHandler.missingRouteErrorHandler(errors));
+app.use(errorHandler.conflictErrorHandler(errors));
+
+app.listen(configs.port, () => {
+	console.log(`Example app listening on port ${configs.port}`);
 });
