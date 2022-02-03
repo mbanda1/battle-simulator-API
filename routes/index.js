887e677e9dd0f5c7fb190230f4962d9f7a425a32
mbanda1 <mbandanixon@gmail.com>
2022-02-03 22:09:11 +0300

    Add game router

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const battle = require('./battle');
 const army = require('./armies');
 const attack = require('./attack.strategy');
+const games = require('./games');
 
 router.use('/battles', [battle]);
 router.use('/armies', [army]);
 router.use('/attack-strategy', [attack]);
+router.use('/games', [games]);
 
 module.exports = router;
