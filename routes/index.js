710eea9a7a8a1c228eac65be54aedd72b8da5e95
mbanda1 <mbandanixon@gmail.com>
2022-02-04 15:10:12 +0300

    Seed index router

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const battle = require('./battle');
 const army = require('./armies');
 const attack = require('./attack.strategy');
 const games = require('./games');
+const seed = require('./runSeed');
 
 router.use('/battles', [battle]);
 router.use('/armies', [army]);
 router.use('/attack-strategy', [attack]);
 router.use('/games', [games]);
+router.use('/seed', [seed]);
 
 module.exports = router;
