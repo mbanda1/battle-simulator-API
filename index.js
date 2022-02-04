37abcc3751b77b07c5962ca9d8b7c2d05eb8babb
mbanda1 <mbandanixon@gmail.com>
2022-02-04 11:34:08 +0300

    Launch Socket on http server

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const socketio = require('socket.io');
+const http = require('http');
 const configs = require('./config/envroment-variables.json');
 
-const express = require('./app');
-const server = require('./httpClinet');
-require('./socketIo');
+const errorHandler = require('./middleware/errorHandler');
+const errors = require('./errors');
+const routes = require('./routes');
+const chat = require('./socketIo');
 
-server.on('request', express);
+const app = express();
+
+const jsonParser = express.json();
+const corsMiddleware = cors();
+
+const server = http.createServer(app);
+const io = socketio(server, {
+	cors: {
+		origin: '*',
+		methods: ['GET', 'POST'],
+	},
+});
+
+app.use(corsMiddleware);
+app.use(jsonParser);
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.use(routes);
+
+app.use(errorHandler.internalServerErrorHandler(errors));
+app.use(errorHandler.missingRouteErrorHandler(errors));
+app.use(errorHandler.conflictErrorHandler(errors));
 
 server.listen(configs.port, () => {
-	console.log(`Battle Simulator listening on port ${configs.port}`);
+	console.log(`Battle Simulator- listening on port ${configs.port}`);
 });
+
+chat(io);
 
 process.on('exit', () => {
 	console.error('Process Ended !');
